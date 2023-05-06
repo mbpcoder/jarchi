30af8de9534b51174963d2259afc0dba68423c56
Mahdi Bagheri <mb.reckless@gmail.com>
2023-05-06 12:52:10 +0330

    add description

--- jira.php
+++ jira.php
@@ -7,7 +7,7 @@ $chatId = '';
 
 $data = json_decode(file_get_contents('php://input'));
 
-if(isset($_GET['debug'])){
+if (isset($_GET['debug'])) {
     file_put_contents(getRandomName(), json_encode($data));
 }
 
@@ -16,10 +16,10 @@ $domain = parse_url($data?->issue?->self, PHP_URL_SCHEME) . "://" . parse_url($d
 $message = '';
 
 $userUrl = $domain . '/secure/ViewProfile.jspa?name=' . $data?->user?->name;
-$message .= '<b><a href="'.$userUrl.'">' . $data?->user?->displayName . '</a></b> => ';
+$message .= '<b><a href="' . $userUrl . '">' . $data?->user?->displayName . '</a></b> => ';
 
 $projectUrl = $domain . '/projects/' . $data?->issue?->fields?->project?->key;
-$message .= '<a href="' . $projectUrl . '">'.$data?->issue?->fields?->project?->name.'</a>' . PHP_EOL;
+$message .= '<a href="' . $projectUrl . '">' . $data?->issue?->fields?->project?->name . '</a>' . PHP_EOL;
 
 $message .= $data?->issue?->fields?->project?->name . PHP_EOL;
 
@@ -33,7 +33,11 @@ if (!empty($data?->issue?->fields?->assignee->displayName)) {
 
 if (!empty($data?->issue?->fields?->summary)) {
     $cardUrl = $domain . '/browse/' . $data?->issue?->key;
-    $message .= '<b><a href="' . $cardUrl . '">Summery: </a></b>' . $data?->issue?->fields?->summary . PHP_EOL;
+    $message .= '<b><a href="' . $cardUrl . '">Summery: </a></b>' . strip_tags($data?->issue?->fields?->summary) . PHP_EOL;
+}
+
+if (!empty($data?->issue?->fields?->description)) {
+    $message .= '<b>Description: </b>' . strip_tags($data?->issue?->fields?->description) . PHP_EOL;
 }
 
 echo 'success';
