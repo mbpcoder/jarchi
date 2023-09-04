366332d5332c7fa1de1a889801ff28d3558f5109
Mahdi Bagheri <mb.programmer@yahoo.com>
2023-09-04 10:20:41 +0330

    start crisp

---
+++ src/crisp.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$botToken = config('CRISP_BOT_TOKEN');
+$chatId = config('CRISP_CHAT_ID');
+$debugMode = config('DEBUG', false);
+
+$data = json_decode(file_get_contents('php://input'));
+
+if ($debugMode) {
+    file_put_contents(getRandomName(), json_encode($data));
+}
+
+if (is_object($data) && isset($data->data)) {
+
+    // send to telegram
+    $message = '<b>' . $data->data->user->nickname . '</b>' . PHP_EOL;
+    $message .= $data->data->content . PHP_EOL;
+    sendToTelegramBot($botToken, $chatId, $message);
+}
+
+echo 'success';
