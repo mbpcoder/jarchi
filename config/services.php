02960e9779fecda9225853aa98146538980c4d0c
Mahdi Bagheri <mb.programmer@yahoo.com>
2023-12-09 01:27:30 +0330

    simpler .env file loader

--- config/services.php
+++ config/services.php
@@ -2,7 +2,7 @@
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/jarchi');
+$dotenv = Dotenv::createImmutable('..');
 $dotenv->load();
 
 $dotenv->ifPresent('DEBUG')->isBoolean();
